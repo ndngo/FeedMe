2eac35b5b2e58a86d71d0570ca287261e4108b32
ndngo <ngonathanieldean@gmail.com>
2017-12-06 01:00:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mehungry/feedme/ViewAccountSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.example.mehungry.feedme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewAccountSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_account_settings);
+    }
+}
---
+++ app/src/main/java/com/example/mehungry/feedme/ViewOptionsActivity.java
@@ -0,0 +1,13 @@
+package com.example.mehungry.feedme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewOptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_options);
+    }
+}
