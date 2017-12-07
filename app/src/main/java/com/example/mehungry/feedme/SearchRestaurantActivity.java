2820552fe1ff8dc6b4cedbdbd6fa31cf12adb24d
ndngo <ngonathanieldean@gmail.com>
2017-12-06 21:33:06 -0500

    Added PlacePicker

--- app/src/main/java/com/example/mehungry/feedme/SearchRestaurantActivity.java
+++ app/src/main/java/com/example/mehungry/feedme/SearchRestaurantActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class SearchRestaurantActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_restaurant);
+
     }
 }
